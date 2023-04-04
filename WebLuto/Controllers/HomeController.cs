8d1f92b46f1f9722e2e784f357af16c846d2e9e3
RenanLima7 <renanlima.developer@gmail.com>
2023-04-04 19:32:24 -0300

    redirecionamento swagger

---
+++ WebLuto/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebLuto.Controllers
+{
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public RedirectResult Index()
+        {
+            return Redirect("/swagger/index.html");
+        }
+    }
+}
